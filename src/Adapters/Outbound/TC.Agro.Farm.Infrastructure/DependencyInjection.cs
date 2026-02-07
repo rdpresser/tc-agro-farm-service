4e4f349ed6f07081b36167bce5dcf6007d82c413
Ricardo <ricardocavalcantesilva@gmail.com>
2026-02-07 18:55:24 -0300

    feat: ajuste do applymigration

--- src/Adapters/Outbound/TC.Agro.Farm.Infrastructure/DependencyInjection.cs
+++ src/Adapters/Outbound/TC.Agro.Farm.Infrastructure/DependencyInjection.cs
@@ -2,6 +2,9 @@ using TC.Agro.Farm.Application.Abstractions.Ports;
 using TC.Agro.Farm.Infrastructure.Messaging;
 using TC.Agro.Farm.Infrastructure.Repositories;
 using TC.Agro.SharedKernel.Infrastructure;
+using TC.Agro.SharedKernel.Infrastructure.Database;
+using TC.Agro.SharedKernel.Infrastructure.Database.EfCore;
+using Wolverine.EntityFrameworkCore;
 
 namespace TC.Agro.Farm.Infrastructure
 {
@@ -52,6 +55,9 @@ namespace TC.Agro.Farm.Infrastructure
                 }
 
             });
+
+            // IApplicationDbContext (required for SharedKernel ApplyMigrations)
+            services.AddScoped<IApplicationDbContext>(sp => sp.GetRequiredService<ApplicationDbContext>());
 
             // Unit of Work (for simple handlers that don't need outbox)
             services.AddScoped<IUnitOfWork>(sp => sp.GetRequiredService<ApplicationDbContext>());
